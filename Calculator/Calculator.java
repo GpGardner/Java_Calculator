838cbd4af0f7c8fdd0c5e97d1168db588255eb7b
George Gardner <gpgardner@yahoo.com>
2020-01-30 17:52:54 -0500

    Added ability to call a single variable and to use some different trig functions on it

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -6,29 +6,64 @@ public class Calculator {
     public int num2;
 
 
+    public Calculator(int num1) {
+        this.num1 = num1;
+    }
     public Calculator(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
 
     public int addition() {
-        System.out.printf("%s + %s = ",num1, num2);
+        System.out.printf("%d + %d = ",num1, num2);
         return num1+num2;
 
     }
 
     public int subtraction() {
-        System.out.printf("%s - %s = ",num1, num2);
+        System.out.printf("%d - %d = ",num1, num2);
         return num1-num2;
     }
 
     public int multiplication() {
-        System.out.printf("%s * %s = ",num1, num2);
+        System.out.printf("%d * %d = ",num1, num2);
         return num1*num2;
     }
 
     public int division() {
-        System.out.printf("%s / %s = ",num1, num2);
+        System.out.printf("%d / %d = ",num1, num2);
         return num1/num2;
+    }
+
+    public int square() {
+        System.out.printf("%d squared = ", num1);
+        return num1*num1;
+    }
+
+    public int factorial() {
+        int total = 1;
+        for (int i = 1; i <= num1; i++) {
+            total *= i;
+            if (i < num1) {
+                System.out.print(i + " x ");
+            }
+        }
+        System.out.printf("%d's factorial = ", num1);
+        return total;
+    }
+
+    public double sine() {
+        System.out.printf("Angle %d's sine  = ", num1);
+        return Math.sin(num1);
+    }
+
+    public double cosine() {
+        System.out.printf("Angle %d's cosine = ", num1);
+        return Math.cos(num1);
+    }
+
+    public double tangent() {
+        System.out.printf("Angle %d's tangent = ", num1);
+        return Math.tan(num1);
     }
 }
--- Calculator/MainMethod.java
+++ Calculator/MainMethod.java
@@ -10,38 +10,97 @@ public class MainMethod {
 
 
         do {
-        System.out.println("Enter First Number");
-        int num1 = input.nextInt();
 
-        System.out.println("Enter Second Number");
-        int num2 = input.nextInt();
-
-            System.out.println("What would you like to do with your numbers? Enter: 1 - Add, 2 - Subtract, 3 - Multiply, 4 - Divide");
+            System.out.println("What would you like to do?\nEnter: 1 - Add, 2 - Subtract, 3 - Multiply, 4 - Divide, " +
+                               "5 - Square, 6 - Factorial, 7 - Sine, 8 - Cosine, 9 - Tangent");
             int choice = input.nextInt();
 
-            Calculator calc = new Calculator(num1, num2);
 
             switch (choice) {
                 case 1: //call function for Addition
-                    System.out.println(calc.addition());
+                    System.out.println("Enter first number");
+                    int add1 = input.nextInt();
+
+                    System.out.println("Enter second number");
+                    int add2 = input.nextInt();
+
+                    Calculator addCalc = new Calculator(add1, add2);
+
+                    System.out.println(addCalc.addition());
                     break;
                 case 2: //call function for Subtraction
-                    System.out.println(calc.subtraction());
+                    System.out.println("Enter first number");
+                    int sub1 = input.nextInt();
+
+                    System.out.println("Enter second number");
+                    int sub2 = input.nextInt();
+
+                    Calculator subCalc = new Calculator(sub1, sub2);
+                    System.out.println(subCalc.subtraction());
                     break;
                 case 3: //call function for Multiplication
-                    System.out.println(calc.multiplication());
+                    System.out.println("Enter first number");
+                    int mul1 = input.nextInt();
+
+                    System.out.println("Enter second number");
+                    int mul2 = input.nextInt();
+
+                    Calculator mulCalc = new Calculator(mul1, mul2);
+                    System.out.println(mulCalc.multiplication());
                     break;
                 case 4: //call function for Division
-                    System.out.println(calc.division());
+                    System.out.println("Enter first number");
+                    int div1 = input.nextInt();
+
+                    System.out.println("Enter second number");
+                    int div2 = input.nextInt();
+
+                    Calculator divCalc = new Calculator(div1, div2);
+                    System.out.println(divCalc.division());
+                    break;
+                case 5: //call function for Square
+                    System.out.println("Enter the number to be squared");
+                    int squ1 = input.nextInt();
+
+                    Calculator squCalc = new Calculator(squ1);
+                    System.out.println(squCalc.square());
+                    break;
+                case 6: //call for factorial
+                    System.out.println("Enter number to see its factorial");
+                    int fac1 = input.nextInt();
+
+                    Calculator facCalc = new Calculator(fac1);
+                    System.out.println(facCalc.factorial());
+                    break;
+                case 7: //call for sine
+                    System.out.println("Enter angle to have its sine calculated");
+                    int sin1 = input.nextInt();
+
+                    Calculator sinCalc = new Calculator(sin1);
+                    System.out.println(sinCalc.sine());
+                    break;
+                case 8: //call for cosine
+                    System.out.println("Enter angle to have its sine calculated");
+                    int cos1 = input.nextInt();
+
+                    Calculator cosCalc = new Calculator(cos1);
+                    System.out.println(cosCalc.cosine());
+                    break;
+                case 9: //call for tangent
+                    System.out.println("Enter angle to have its sine calculated");
+                    int tan1 = input.nextInt();
+
+                    Calculator tanCalc = new Calculator(tan1);
+                    System.out.println(tanCalc.tangent());
                     break;
                 default:
-                    System.out.println("Please enter 1, 2, 3, or 4");
+                    System.out.println("Please enter 1-9");
             }
 
             System.out.println("Do you want to continue: ");
             input.nextLine();
             runAgain = input.nextLine();
 
-        } while(runAgain.equals("y"));
+        }   while(runAgain.equals("y"));
     }
 }
